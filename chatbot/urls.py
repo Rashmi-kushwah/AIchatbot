6cf82ad611f206b40725362a110eff042e0e162e
Rashmi Kushwah <rashmiinfo6@gmail.com>
2025-03-01 13:56:18 +0530

    Removed secret API key

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -6,5 +6,5 @@ from .views import chatbot_response,get_csrf_token,chatbot_page
 urlpatterns = [
     path("chatbot-response/", chatbot_response, name="chatbot_response"),
     path("get-csrf-token/", get_csrf_token, name="get_csrf_token"),
-    path("/", chatbot_page, name="chatbot_page"),
+    path("chatbot/", chatbot_page, name="chatbot_page"),
 ]
